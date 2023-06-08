5ffe8aeb6b53904241e471fb4b51cfa83ccb9150
Niculin Steiner <niculin.steiner@ergon.ch>
2023-06-08 07:17:41 +0000

    add taskController

---
+++ taskApi/controller/taskController.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json({ extended: true }));
+
+
+module.exports = router;
---
+++ taskApi/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const taskController = require('./controller/taskController');
+
+const app = express;
+const port = 3003;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ extended: true }));
+app.use(express.json());
+app.use('/tasks', taskController);
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+  
